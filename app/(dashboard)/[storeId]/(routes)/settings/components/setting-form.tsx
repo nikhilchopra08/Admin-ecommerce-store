2ac4d706be5e17c1ed07847bf70d534c3e554534
Nikhil chopra <nikhil0080012005@gmail.com>
2024-07-19 21:53:30 +0530

    schema for bill board

--- app/(dashboard)/[storeId]/(routes)/settings/components/setting-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/setting-form.tsx
@@ -7,6 +7,7 @@ import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "
 import { Heading } from "@/components/ui/heading";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
+import { useOrigin } from "@/hooks/use-origin";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Store } from "@prisma/client";
 import axios from "axios";
@@ -35,6 +36,7 @@ export const SettingsForm : React.FC<SettingsFormProps> = ({
     const [loading, setLoading] = useState(false);
     const params = useParams();
     const router = useRouter();
+    const origin = useOrigin();
 
     const form = useForm<SettingsFormValues>({
         resolver: zodResolver(formSchema),
