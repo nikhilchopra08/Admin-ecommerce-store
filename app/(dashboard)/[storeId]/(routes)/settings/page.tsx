c3e7b8271995fc4d8cca4884cfba63c73382002c
Nikhil chopra <nikhil0080012005@gmail.com>
2024-07-20 01:00:36 +0530

    Added workflow

--- app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -33,7 +33,7 @@ const SettingsPage: React.FC<SettingsPageProps> = async ({
   return (
     <div className='flex-col'>
         <div className='flex-1 space-y-4 p-8 pt-6'>
-            <SettingsForm initalData={store}/>
+            <SettingsForm initialData={store}/>
 
         </div>
     </div>
