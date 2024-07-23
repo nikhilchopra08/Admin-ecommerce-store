7dacf2681dd8f7939ea41f79ad98422e789ef0ed
Nikhil chopra <nikhil0080012005@gmail.com>
2024-07-23 14:52:57 +0530

    corrected typo

--- app/(dashboard)/[storeId]/(routes)/billboards/[BillBoardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[BillBoardId]/page.tsx
@@ -2,9 +2,19 @@ import prismadb from "@/lib/prismadb";
 import { BillboardForm } from "./components/billboard-form";
 
 const BillboardPage = async ({ params }: { params: { billboardId: string } }) => {
+    const { billboardId } = params;
+
+    if (!billboardId) {
+        throw new Error('Billboard ID is missing');
+    }
+
     const billboard = await prismadb.billboard.findUnique({ 
-        where: { id: params.billboardId }
+        where: { id: billboardId }
     });
+
+    if (!billboard) {
+        throw new Error('Billboard not found');
+    }
 
     return (
         <div className="flex-col">
@@ -13,6 +23,6 @@ const BillboardPage = async ({ params }: { params: { billboardId: string } }) =>
             </div>
         </div>
     );
-}
+};
 
 export default BillboardPage;
